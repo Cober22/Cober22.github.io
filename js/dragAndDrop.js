a062ad6e561152a4f48ffcfebb7f1d347cb9ba2c
Cober-t <al385745@uji.es>
2023-03-24 13:08:04 +0100

    Test 1

--- js/dragAndDrop.js
+++ js/dragAndDrop.js
@@ -1,79 +1,79 @@
-let uploadButton = document.getElementById("upload-button");
-    chosenImage  = document.getElementById("chosen-image");
-    fileName     = document.getElementById("file-name");
-    container    = document.querySelector(".container");
-    error        = document.getElementById("error");
-    pdfDisplay   = document.getElementById("pdf-display");
-    inputText    = document.getElementById("inputText");
+// let uploadButton = document.getElementById("upload-button");
+//     chosenImage  = document.getElementById("chosen-image");
+//     fileName     = document.getElementById("file-name");
+//     container    = document.querySelector(".container");
+//     error        = document.getElementById("error");
+//     pdfDisplay   = document.getElementById("pdf-display");
+//     inputText    = document.getElementById("inputText");
 
-const fileHandler = (file, name, type) => {
-  if (type.split("/")[1] !== "pdf") {
-    //File Type Error
-    error.innerText = "Please upload a pdf file";
-    return false;
-  }
-  error.innerText = "";
-  let reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = () => {
-    //image and file name
-    let pdfContainer = document.createElement("figure");
-    let pdf = document.createElement("pdf");
-    pdf.src = reader.result;
-    pdfContainer.appendChild(pdf);
-    pdfContainer.innerHTML += `<figcaption>${name}</figcaption>`;
-    pdfDisplay.appendChild(pdfContainer);
-  };
-};
+// const fileHandler = (file, name, type) => {
+//   if (type.split("/")[1] !== "pdf") {
+//     //File Type Error
+//     error.innerText = "Please upload a pdf file";
+//     return false;
+//   }
+//   error.innerText = "";
+//   let reader = new FileReader();
+//   reader.readAsDataURL(file);
+//   reader.onloadend = () => {
+//     //image and file name
+//     let pdfContainer = document.createElement("figure");
+//     let pdf = document.createElement("pdf");
+//     pdf.src = reader.result;
+//     pdfContainer.appendChild(pdf);
+//     pdfContainer.innerHTML += `<figcaption>${name}</figcaption>`;
+//     pdfDisplay.appendChild(pdfContainer);
+//   };
+// };
 
-//Upload Button
-pdfDisplay.addEventListener("change", () => {
-  imageDisplay.innerHTML = "";
-  Array.from(uploadButton.files).forEach((file) => {
-    fileHandler(file, file.name, file.type);
-  });
-});
+// //Upload Button
+// pdfDisplay.addEventListener("change", () => {
+//   imageDisplay.innerHTML = "";
+//   Array.from(uploadButton.files).forEach((file) => {
+//     fileHandler(file, file.name, file.type);
+//   });
+// });
 
-container.addEventListener("dragenter", (e) => 
-    {
-        e.preventDefault();
-        e.stopPropagation();
-        container.classList.add("active");
-    },false);
+// container.addEventListener("dragenter", (e) => 
+//     {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         container.classList.add("active");
+//     },false);
 
-container.addEventListener("dragleave", (e) => 
-    {
-        e.preventDefault();
-        e.stopPropagation();
-        container.classList.remove("active");
-    }, false);
+// container.addEventListener("dragleave", (e) => 
+//     {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         container.classList.remove("active");
+//     }, false);
   
-container.addEventListener("dragover", (e) => 
-    {
-        e.preventDefault();
-        e.stopPropagation();
-        container.classList.add("active");
-    }, false);
+// container.addEventListener("dragover", (e) => 
+//     {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         container.classList.add("active");
+//     }, false);
 
 
-let fileContainer = file
-container.addEventListener("drop", (e) => 
-    {
-        e.preventDefault();
-        e.stopPropagation();
-        container.classList.remove("active");
-        let draggedData = e.dataTransfer;
-        let files = draggedData.files;
-        pdfDisplay.innerHTML = "";
-        Array.from(files).forEach((file) => {
-            fileHandler(file, file.name, file.type);
-        });
-    },false);
+// let fileContainer = file
+// container.addEventListener("drop", (e) => 
+//     {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         container.classList.remove("active");
+//         let draggedData = e.dataTransfer;
+//         let files = draggedData.files;
+//         pdfDisplay.innerHTML = "";
+//         Array.from(files).forEach((file) => {
+//             fileHandler(file, file.name, file.type);
+//         });
+//     },false);
 
 
-window.onload = () => {
-  error.innerText = "";
-};
+// window.onload = () => {
+//   error.innerText = "";
+// };
 
 function sendEmail() {
     Email.send({
@@ -84,11 +84,11 @@ function sendEmail() {
         From: "jorgetejadolopez@gmail.com",
         Subject: "Sending Email using javascript",
         Body: inputText.innerText,
-        Attachments: [
-        {
-            name: "File_Name_with_Extension",
-            path: "asdasd"
-        }],
+        // Attachments: [
+        // {
+        //     name: "File_Name_with_Extension",
+        //     path: "asdasd"
+        // }],
         }).then(function (message) {
             alert("mail sent successfully")
         })
